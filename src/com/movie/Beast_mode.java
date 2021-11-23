bbd09307215396b970c3e88c9ebecd7cf3941a38
Monishaamon <shamoni033@gmail.com>
2021-11-23 21:20:05 +0530

    Movie_project

---
+++ src/com/movie/Beast_mode.java
@@ -0,0 +1,40 @@
+package com.movie;
+
+public class Beast_mode {
+	
+	private void heroName() {
+		System.out.println("hero name is vijay");
+}
+	
+	private void heroineName() {
+		System.out.println("heroine name is pooja hedge");
+	}
+	private void directorName() {
+		System.out.println("director name is nelson");
+}
+	private void musicDirectorName() {
+		System.out.println("music director name is anirudh");
+}
+	
+	public static void main(String[] args) {
+		//classname object = new classname();
+		Beast_mode d = new Beast_mode();
+		d.heroineName();
+		d.heroName();
+		d.directorName();
+		d.musicDirectorName();
+		
+	}
+	
+}
+
+		
+		
+		
+		
+	
+	
+
+	
+
+
